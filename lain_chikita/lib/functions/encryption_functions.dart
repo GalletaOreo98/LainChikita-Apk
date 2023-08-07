6842661aa4947b74906c3988aca0b863527ed5a9
GalletaOreo98 <christophergironcabrera@gmail.com>
2023-08-06 18:40:16 -0600

    Update - Change AES mode

--- lain_chikita/lib/functions/encryption_functions.dart
+++ lain_chikita/lib/functions/encryption_functions.dart
@@ -3,14 +3,14 @@ import 'package:encrypt/encrypt.dart' as encrypt;
 String encryptData(String jsonData, String secretKey) {
   final key = encrypt.Key.fromUtf8(secretKey);
   final iv = encrypt.IV.fromSecureRandom(16);
-  final encrypter = encrypt.Encrypter(encrypt.AES(key));
+  final encrypter = encrypt.Encrypter(encrypt.AES(key, mode: encrypt.AESMode.cbc));
   final encrypted = encrypter.encrypt(jsonData, iv: iv);
   return encrypted.base16 + iv.base16;
 }
 
 String decryptData(String encryptedData, String secretKey) {
   final key = encrypt.Key.fromUtf8(secretKey);
-  final encrypter = encrypt.Encrypter(encrypt.AES(key));
+  final encrypter = encrypt.Encrypter(encrypt.AES(key, mode: encrypt.AESMode.cbc));
   final iv16 = encryptedData.substring(32, 64);
   final iv = encrypt.IV.fromBase16(iv16);
   encryptedData = encryptedData.substring(0, 32);
