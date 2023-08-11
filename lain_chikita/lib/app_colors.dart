39085e095885b8fc6548e831b63c7d3fd9749a98
GalletaOreo98 <christophergironcabrera@gmail.com>
2023-08-11 02:10:32 -0600

    Add - Gacha screen prototype

--- lain_chikita/lib/app_colors.dart
+++ lain_chikita/lib/app_colors.dart
@@ -4,6 +4,7 @@ import 'dart:ui';
 const darkBackground = Color.fromARGB(255, 56, 53, 59);
 const mediumBackground = Color.fromARGB(255, 87, 71, 71);
 const lightBackground = Color.fromARGB(255, 87, 71, 71);
+const cutePink = Color.fromARGB(255, 248, 187, 208);
 
 //Colores de Texto
 const secondaryLightText = Color.fromARGB(255, 173, 173, 173);
--- lain_chikita/lib/screens/gacha_screen.dart
+++ lain_chikita/lib/screens/gacha_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/services.dart';
 
 // ignore: library_prefixes
 import '../app_colors.dart' as MY_APP_COLORS;
@@ -13,27 +14,63 @@ class GachaScreen extends StatefulWidget {
 }
 
 class MyWidgetState extends State<GachaScreen> {
-
   final _player = AudioPlayer(playerId: 'selectAccessory');
+  String _copiedText = '';
+  String uuidToUse = '';
 
   Future<void> playSelectAccessorySound() async {
     await _player.play(AssetSource("audio/select_accessory_sound.mp3"));
   }
 
+  void _copyText() {
+    Clipboard.setData(ClipboardData(text: userUuid));
+    setState(() {
+      _copiedText = '¡Copiado!';
+    });
+  }
+
+  void setUuidToUse() {}
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      //alignment: Alignment.center,
-      color: MY_APP_COLORS.darkBackground,
-      child: 
-        Stack(
-          children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: TextField(decoration: InputDecoration(labelText: userUuid),),
-            )
-          ],
-        ),
-    );
+        color: MY_APP_COLORS.darkBackground,
+        child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                      Color.fromARGB(255, 255, 87, 87), // Cambia el color de fondo aquí
+                  ),
+                  onPressed: _copyText,
+                  child: const Text('Copiar mi UUID'),
+                ),
+                const SizedBox(height: 1),
+                Text(
+                  _copiedText,
+                  style: const TextStyle(
+                      fontSize: 10.0,
+                      fontFamily: 'monospace',
+                      color: Colors.white70,
+                      fontStyle: FontStyle.italic),
+                ),
+                TextFormField(
+                  initialValue: uuidToUse,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.amber, fontSize: 15.0),
+                  decoration: const InputDecoration(
+                    labelText: 'Pega un UUID',
+                    labelStyle: TextStyle(color: Colors.white),
+                    floatingLabelAlignment: FloatingLabelAlignment.center,
+                  ),
+                  onFieldSubmitted: (value) =>
+                      setState(() => {uuidToUse = value, setUuidToUse()}),
+                ),
+              ],
+            )));
   }
 }
