413cc347afbe12261070957d12351ab33084ff00
GalletaOreo98 <christophergironcabrera@gmail.com>
2023-08-11 22:21:52 -0600

    Fix - now encryption + iv for larger data works

--- lain_chikita/lib/functions/encryption_functions.dart
+++ lain_chikita/lib/functions/encryption_functions.dart
@@ -7,15 +7,15 @@ String encryptData(String jsonData, String secretKey) {
   final iv = encrypt.IV.fromSecureRandom(16);
   final encrypter = encrypt.Encrypter(encrypt.AES(key, mode: encrypt.AESMode.cbc));
   final encrypted = encrypter.encrypt(jsonData, iv: iv);
-  return encrypted.base16 + iv.base16;
+  return iv.base16 + encrypted.base16;
 }
 
 String decryptData(String encryptedData, String secretKey) {
   final key = encrypt.Key.fromUtf8(secretKey);
   final encrypter = encrypt.Encrypter(encrypt.AES(key, mode: encrypt.AESMode.cbc));
-  final iv16 = encryptedData.substring(32, 64);
+  final iv16 = encryptedData.substring(0, 32);
   final iv = encrypt.IV.fromBase16(iv16);
-  encryptedData = encryptedData.substring(0, 32);
+  encryptedData = encryptedData.substring(32);
   final encrypted = encrypt.Encrypted.fromBase16(encryptedData);
   final decrypted = encrypter.decrypt(encrypted, iv: iv);
   return decrypted;
